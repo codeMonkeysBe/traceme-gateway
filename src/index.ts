639480d7850f9c93499b2e0cd9ed0bccc9934f0d
Niki Van Cleemput <niki@waasit.be>
2017-06-26 21:10:44 +0200

    start of a new beginning

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as zmq from "zmq";
 import * as uuid from "uuid";
 
+import { logger } from "./lib/logger";
+
+logger.level = "silly";
+
 var dealer = zmq.socket('dealer');
 
-dealer.identity = `${uuid.v4()}-traceme-gateway`;
+dealer.identity = `pid://${process.pid}/traceme-gateway`;
 
 dealer.connect('tcp://127.0.0.1:3000');
 
@@ -13,5 +17,6 @@ dealer.on('message', (...frames) => {
   let decoded: string[] = frames.map((frame: Buffer) => {
     return frame.toString();
   });
-  console.log('message', dealer.identity, decoded);
+  dealer.send('doing it again');
+  logger.debug('message', dealer.identity, decoded);
 });
